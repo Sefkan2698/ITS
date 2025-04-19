743f634a213b10c7ac1f49ecff14856ef61a1763
Sefkan2698 <sakaits@gmx.de>
2025-04-19 21:11:38 +0200

    Fixed app configuration files

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,11 +1,3 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { appConfig } from './app.config';
+import { Routes } from '@angular/router';
 
-const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering()
-  ]
-};
-
-export const config = mergeApplicationConfig(appConfig, serverConfig);
+export const serverRoutes: Routes = [];
