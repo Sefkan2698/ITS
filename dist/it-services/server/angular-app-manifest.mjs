f2daedcf198cec0b3a1e4c13a7ac120ec6f354d4
Sefkan2698 <sakaits@gmx.de>
2025-04-19 21:23:58 +0200

    Initial commit

--- dist/it-services/server/angular-app-manifest.mjs
+++ dist/it-services/server/angular-app-manifest.mjs
@@ -33,13 +33,13 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 6730, hash: '9a89bdbca09003c25739747c6eb5c92d6076afe94516a1e051edd5df3c9eccf6', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1729, hash: 'd052576865e71c9ff6dc54d624cc15fefd46bfdc44ce865c5ef33d2b9360c7ef', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'services/index.html': {size: 31134, hash: 'e6b6ca45eb096ff6eb8922c9953b3f6736a48c7d712b8539b2ab0d4bcc08748d', text: () => import('./assets-chunks/services_index_html.mjs').then(m => m.default)},
-    'pricing/index.html': {size: 25142, hash: 'c8c2d911d058886ebcd79e2d2dd19aa01bad85d71e2b96ccec1b87eb3fa39f96', text: () => import('./assets-chunks/pricing_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 27574, hash: '06449e8cc17b080c224b45e00c407800db0e915afd190a6871c50e4271e56101', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'about/index.html': {size: 29453, hash: '85e9eeaf178a57bb4e04f26645c6eb87cde9284aa5fd89ffe85939346e425354', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 28658, hash: '346c1f9b267cd18f139b0ed2f8e2fecd73fcaf42f6bb2b02c4efce7820cd17fb', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 6690, hash: '54eaf822dc5ac4d4d6a1c7d85d751e69b1deff4166eca71f3d2da1ccf1faed95', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1697, hash: 'ecc286dc7197b917b8d853ab7e3fde943dc917e875a3581f8acfd86c1fcd5607', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'services/index.html': {size: 31094, hash: 'd4f0e474e0a3860a2aa4d04bd46def2775a79cdf0c9381389ea83a19e12ad214', text: () => import('./assets-chunks/services_index_html.mjs').then(m => m.default)},
+    'about/index.html': {size: 29413, hash: 'b16f129f33a45816c023b83602299cb0000c7b70d7a20c17fff2044d91714069', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 27534, hash: '8cbba0d1a9d43dbb850cc15d934a1e6183db7eca774699f2644891b9e331f924', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 28618, hash: 'b2a2e640fd6e2f8785c57ede5f994396acd4b199e3b4f81e96f1a7574032c0cc', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'pricing/index.html': {size: 25102, hash: 'b271f40609176b0e772a66f6fda8461cfbdac8bea956b22f8a9e8bb67d4cc545', text: () => import('./assets-chunks/pricing_index_html.mjs').then(m => m.default)},
     'styles-HXF4TWH2.css': {size: 304751, hash: 'BRKnlPILCIs', text: () => import('./assets-chunks/styles-HXF4TWH2_css.mjs').then(m => m.default)}
   },
 };
