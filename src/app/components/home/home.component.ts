fa76d807c946e4e14fb7a6232e33717089392630
Sefkan2698 <sakaits@gmx.de>
2025-04-19 20:06:03 +0200

    Fixed button navigation and added icon CDNs

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,5 +9,7 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+// home.component.ts
 export class HomeComponent {
+  logoPath = 'assets/Saka_IT-Solution.png';
 }
