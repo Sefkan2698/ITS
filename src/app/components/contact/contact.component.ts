749e9c8ff3db8b500e08b6563624218799f9eca9
Sefkan2698 <sakaits@gmx.de>
2025-04-19 21:02:33 +0200

    Fixed configuration files

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule], // RouterLink entfernt
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
