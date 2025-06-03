6a522e1b810bf6e16e2712de98e77724c24a5663
Sefkan Saka <sakaits@gmx.de>
2025-06-03 21:40:14 +0200

    fix txt robot

--- dist/it-services/server/angular-app-manifest.mjs
+++ dist/it-services/server/angular-app-manifest.mjs
@@ -2,44 +2,44 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/sakaits/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/sakaits"
   },
   {
     "renderMode": 2,
-    "route": "/services"
+    "route": "/sakaits/services"
   },
   {
     "renderMode": 2,
-    "route": "/about"
+    "route": "/sakaits/about"
   },
   {
     "renderMode": 2,
-    "route": "/pricing"
+    "route": "/sakaits/pricing"
   },
   {
     "renderMode": 2,
-    "route": "/contact"
+    "route": "/sakaits/contact"
   },
   {
     "renderMode": 2,
-    "redirectTo": "/",
-    "route": "/**"
+    "redirectTo": "/sakaits",
+    "route": "/sakaits/**"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 6674, hash: 'bc06318291f6afef25b29903d217fd4429662a0cd1fd3d26a644356daab0138f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1681, hash: '2d0df5a971b3c44df56a8e6aa613c50e35e30a97936536228ac6516dff6f987b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'services/index.html': {size: 37878, hash: 'b3452add9e3bb263d18cb1a400f451f149ab62c017a5541e51db72037711dfdb', text: () => import('./assets-chunks/services_index_html.mjs').then(m => m.default)},
-    'about/index.html': {size: 33284, hash: '7b58804de3d6c535ee287a7b8c199b8fcfcbdb1628b4f68b580a91fbfcbb149f', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 32639, hash: '80cc40bb3debcb4839ff7389266055311adbf60cd0b2fc094b75c1bc951a74f4', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
-    'pricing/index.html': {size: 29181, hash: '3798a712a7f4a9d2cdfc98f5e76c6915bf4d1a67fa9676572e41ced1f43eaef8', text: () => import('./assets-chunks/pricing_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 32529, hash: 'ad0bf8e99b18878f485e0e6778c5527945c1473932b2425d3a4523a0342347e2', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 6682, hash: 'bf121395b2b6ed2147be77e68a41c6661ffb7b5e0c8f50f0d249c5b8c59e8450', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1689, hash: '18dcecd3d6990265d54e954cfc3ba0986206c4ae220f091d0aad44d88a2c7c7b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'services/index.html': {size: 37958, hash: '713406d11f315e1a64f95732af39482d49e3e08cdcb50f4afa78119ee662c481', text: () => import('./assets-chunks/services_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 32617, hash: '374d83160f9f0a3672203fed9767c90d657cb18471fdc305cc5edb3198e1e824', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'pricing/index.html': {size: 29261, hash: '0555c6052b69e2e67d2c4136b3c8b0de1c30575c4272d0c62f51710f2ed57c35', text: () => import('./assets-chunks/pricing_index_html.mjs').then(m => m.default)},
+    'about/index.html': {size: 33364, hash: '5bdb1507705d19763c74e35c3db4f5ead8c2ad93e9a0b63ac4c2de9def669665', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 32711, hash: '2308b6bda946fcc61dc8c69c6803342924cc07d47e9afd46ff05e19ded1cab16', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
     'styles-HXF4TWH2.css': {size: 304751, hash: 'BRKnlPILCIs', text: () => import('./assets-chunks/styles-HXF4TWH2_css.mjs').then(m => m.default)}
   },
 };
