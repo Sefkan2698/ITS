9c00845ee171c49dff01ba402cf1d4bea3609d33
Sefkan Saka <sakaits@gmx.de>
2025-06-03 21:00:17 +0200

    SEO

--- dist/it-services/server/angular-app-manifest.mjs
+++ dist/it-services/server/angular-app-manifest.mjs
@@ -33,13 +33,13 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 6674, hash: '9e7fae075d7677aadf1bd48923eb4a55d834433e2f450e175e32dab962aed045', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1681, hash: '12aae55ad8370b456a02a78e5f1cdb0da682f960f2259e6c74ae73694767c0e9', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'about/index.html': {size: 33051, hash: '6ae35c24cc6f97785ed81e2d2c181797e2f726aca384970de508d08ad90397bb', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
-    'services/index.html': {size: 34812, hash: 'c52a67a902e2df5124acb9c3a340e445263f42a8d10999a170359d4c1242c8b2', text: () => import('./assets-chunks/services_index_html.mjs').then(m => m.default)},
-    'pricing/index.html': {size: 28941, hash: '6c7e78e19506292f20dad5fbcbba0284884bd61c2452fd32ee4669bbafd5e84f', text: () => import('./assets-chunks/pricing_index_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 32399, hash: '43d4703ae8b2de17942da5a2085341ed9fd4c08c7db6c1b9ec796e48829027ba', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 31335, hash: '3883504e7ecf134d39c42bc16268396d34550df286aa006790773330922d7763', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 6674, hash: '43f93e512a85b47cb4f7a8946072943aa951020f127ef0c8e3bd004c152bcbdf', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1681, hash: '8313c7abdf5a38a8b2d575d39e95a21ae07865a2d4bc25b0543b74f0a65dc96c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'services/index.html': {size: 34812, hash: 'e17c36cf6d32cea38eb0c0d5d62e9ae2b5d05a2e00f0ac8b17ce85e5de008d1e', text: () => import('./assets-chunks/services_index_html.mjs').then(m => m.default)},
+    'about/index.html': {size: 33044, hash: '46a6fdcfcd8b6e282932cdc0dc9a85158c301ef4680ab5ef31741d6ba168724e', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 32399, hash: '9bb50f41abd925b1f01ee6fb9a3812e803f85905db404f3926becf3c549c6d0d', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 31335, hash: '5ceca186c2c1054b95123ab7ff3d5fa7a07127979a7e3ba4ec86df9750f12c6e', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'pricing/index.html': {size: 28941, hash: '08f3caf5483e46d501347171c2212d284054ea6a7e270fa55cd142808ceb6f68', text: () => import('./assets-chunks/pricing_index_html.mjs').then(m => m.default)},
     'styles-HXF4TWH2.css': {size: 304751, hash: 'BRKnlPILCIs', text: () => import('./assets-chunks/styles-HXF4TWH2_css.mjs').then(m => m.default)}
   },
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -15,4 +16,12 @@ import { FooterComponent } from './shared/footer/footer.component';
 })
 export class AppComponent {
   title = 'it-services';
+
+  constructor(private titleService: Title, private metaService: Meta) {
+    this.titleService.setTitle('IT-Service Kamp-Lintfort | Saka IT-Solutions – PC & Handy Hilfe');
+    this.metaService.updateTag({
+      name: 'description',
+      content: 'IT-Service in Kamp-Lintfort, Geldern & Goch – PC-Reparatur, Handyhilfe & IT-Schulungen für Senioren. Persönlich, zuverlässig & vor Ort.'
+    });
+  }
 }
