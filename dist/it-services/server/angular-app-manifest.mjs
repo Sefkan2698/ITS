f91841d1dd507cc8392760191adbb262d7dd534b
Sefkan Saka <sakaits@gmx.de>
2025-06-03 18:52:27 +0200

    fix: base href auf / gesetzt

--- dist/it-services/server/angular-app-manifest.mjs
+++ dist/it-services/server/angular-app-manifest.mjs
@@ -2,44 +2,44 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/sakaits/',
+  baseHref: '/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/sakaits"
+    "route": "/"
   },
   {
     "renderMode": 2,
-    "route": "/sakaits/services"
+    "route": "/services"
   },
   {
     "renderMode": 2,
-    "route": "/sakaits/about"
+    "route": "/about"
   },
   {
     "renderMode": 2,
-    "route": "/sakaits/pricing"
+    "route": "/pricing"
   },
   {
     "renderMode": 2,
-    "route": "/sakaits/contact"
+    "route": "/contact"
   },
   {
     "renderMode": 2,
-    "redirectTo": "/sakaits",
-    "route": "/sakaits/**"
+    "redirectTo": "/",
+    "route": "/**"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 6682, hash: 'ec7d614f0c1521963d308b499164d8dbae5f11542035370b799250b3166ef669', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1689, hash: 'fcc29304850f7c22ff1614417c46b64d301bc6fdc7ac5789ab1c89fc90affdaf', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'services/index.html': {size: 34891, hash: '4bad0592288248c5a7d9569c72920c0003ffe84986c57541250e97e4b489abe3', text: () => import('./assets-chunks/services_index_html.mjs').then(m => m.default)},
-    'about/index.html': {size: 33123, hash: 'e5f26b04c1be1e80baa6139d9ec3b94aa45420125e2e6b3ddd6e0f16a8b07d80', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 31423, hash: 'a1d3c5ed593596c76fd851867ecf21c1cfb63ff5688431435dcb244452f6c261', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'pricing/index.html': {size: 29021, hash: '84450ddd892e4b2cbdd86e933594e560c5f4863673f41ea4670c69d260058e69', text: () => import('./assets-chunks/pricing_index_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 32471, hash: 'be624ccbe098e61171821ba34e0d7a59473c7e9f8b26403ea755773109b0ae5e', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 6674, hash: 'b1a82ace3dc0a5826f2bf1e1b5a18a5e9adb9613003445cf0972dde961036dae', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1681, hash: '7e2c3692510e994ef10d9ff6693f55094cec2f540648620a6f97b1b82bab3502', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'services/index.html': {size: 34819, hash: '6904d2873d5948728f02a247d8e727ef2925c54d8450a4e2dc2cfa9c039f0cf9', text: () => import('./assets-chunks/services_index_html.mjs').then(m => m.default)},
+    'pricing/index.html': {size: 28941, hash: '6a6630c3424dfb3ea99b1357d02859ca8aebc045faa205b01e31ccd537d2697f', text: () => import('./assets-chunks/pricing_index_html.mjs').then(m => m.default)},
+    'about/index.html': {size: 33051, hash: '6fdda42e4275eb4d36b89fb2ea8afcf9fb998788f2ba5b2fd1ab81e68a798b00', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 32399, hash: '69e0a7eb0b2f88a71b750528d8b58f32ed88a90ebce435f465dbfdb58ebe71d8', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 31335, hash: '8c2d10278cacfb0c1d0761d28ee10700683ccc3223b3696377866135831cfbb7', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-HXF4TWH2.css': {size: 304751, hash: 'BRKnlPILCIs', text: () => import('./assets-chunks/styles-HXF4TWH2_css.mjs').then(m => m.default)}
   },
 };
--- dist/it-services/server/assets-chunks/index_csr_html.mjs
+++ dist/it-services/server/assets-chunks/index_csr_html.mjs
@@ -3,7 +3,7 @@ export default `<!DOCTYPE html>
 <head>
   <meta charset="utf-8">
   <title>Saka IT-Solutions</title>
-  <base href="/sakaits/">
+  <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
   
